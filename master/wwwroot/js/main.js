3933e1a4d35d8d78effae06866a0e558ce3c7551
anasmalkawy <anas.malkawy1999@gmail.com>
2025-02-16 17:31:53 +0300

    Add project files.

---
+++ master/wwwroot/js/main.js
@@ -0,0 +1,11 @@
+﻿
+
+
+
+const priceRange = document.getElementById('price-range');
+const priceLabel = document.getElementById('price-label');
+
+priceRange.addEventListener('input', (event) => {
+    priceLabel.textContent = event.target.value;
+});
+
