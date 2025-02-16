2551451238f3ea8fe02709a92c253606f9ceb893
anasmalkawy <anas.malkawy1999@gmail.com>
2025-02-16 17:31:53 +0300

    first task

---
+++ master/Models/Master.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace master.Models
+{
+    public class Master : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult about()
+        {
+            return View();
+        }
+        public IActionResult contact()
+        {
+            return View();
+
+        }
+        public IActionResult ask()
+        {
+            return View();
+        }
+        public IActionResult profile()
+        {
+            return View();
+        }
+    }
+}
