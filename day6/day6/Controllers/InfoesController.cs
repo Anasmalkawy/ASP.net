a26e1f50bb184a0f07283c8e4c7b4acb7b7dcd15
anasmalkawy <anas.malkawy1999@gmail.com>
2025-02-23 18:19:40 +0300

    task6

---
+++ day6/day6/Controllers/InfoesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using day6.Models;
+
+namespace day6.Controllers
+{
+    public class InfoesController : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public InfoesController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Info.ToList());
+        }
+
+
+
+
+
+
+       public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create( Info info)
+        {
+                _context.Add(info);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            
+        }
+
+
+
+
+
+
+    }
+}
---
+++ day6/day6/Models/Info.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day6.Models;
+
+public partial class Info
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Email { get; set; }
+
+    public int? Phone { get; set; }
+
+    public string? Address { get; set; }
+}
